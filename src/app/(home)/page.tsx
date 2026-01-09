2f824f097af1b37cdf7666cb68923f5e58a2a2cc
AndrisBorbas <andris.borbas@gmail.com>
2026-01-09 01:39:20 +0100

    retime anims

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -15,26 +15,26 @@ export default function HomePage() {
 				key={animationKey}
 				className="text-primary mt-8 text-center text-6xl font-extrabold tracking-tight sm:text-7xl"
 			>
-				<span className="animate-fade-in-right animation-duration-[2s] relative mr-18 inline-block opacity-0 [animation-delay:1000ms] sm:mr-22">
+				<span className="animate-fade-in-right animation-duration-[2s] relative mr-18 inline-block opacity-0 [animation-delay:100ms] sm:mr-22">
 					Lili
 				</span>
 				<br />
-				<span className="animate-fade-in-zoom animation-duration-[2s] relative inline-block -translate-y-2 opacity-0 [animation-delay:1500ms]">
+				<span className="animate-fade-in-zoom animation-duration-[2s] relative inline-block -translate-y-2 opacity-0 [animation-delay:500ms]">
 					+
 				</span>
 				<br />
-				<span className="animate-fade-in-left animation-duration-[2s] relative ml-18 inline-block opacity-0 [animation-delay:1200ms] sm:ml-22">
+				<span className="animate-fade-in-left animation-duration-[2s] relative ml-18 inline-block opacity-0 [animation-delay:200ms] sm:ml-22">
 					Rudi
 				</span>
 				<br />
 				<span className="text-secondary/50 relative mt-8 inline-block max-w-xl border-0 text-5xl transform-3d sm:text-6xl">
-					<span className="animate-fade-in-zoom opacity-0 [animation-delay:2500ms]">
+					<span className="animate-fade-in-zoom opacity-0 [animation-delay:1500ms]">
 						2026.
 					</span>
-					<span className="animate-fade-in-zoom opacity-0 [animation-delay:2700ms]">
+					<span className="animate-fade-in-zoom opacity-0 [animation-delay:1700ms]">
 						08.
 					</span>
-					<span className="animate-fade-in-zoom opacity-0 [animation-delay:3000ms]">
+					<span className="animate-fade-in-zoom opacity-0 [animation-delay:2000ms]">
 						14.
 					</span>
 				</span>
