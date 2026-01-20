6302a308f4ae2cfd16399b873f3d49332f5cecec
AndrisBorbas <andris.borbas@gmail.com>
2026-01-20 17:51:19 +0100

    footer and live painting info

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export function Footer() {
 	return (
 		<footer className="glass glass-shadow mt-8 w-full py-4">
 			<div className="text-muted-foreground container mx-auto text-center text-sm">
-				&copy; {new Date().getFullYear()} andrisborbas
+				<Button variant="link" size="sm" asChild>
+					<Link href="https://andrisborbas.com" target="_blank">
+						&copy; {new Date().getFullYear()} andrisborbas
+					</Link>
+				</Button>
 			</div>
 		</footer>
 	);
