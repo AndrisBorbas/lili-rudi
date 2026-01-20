a028804aef60db1c4e3a599ff9a86851bbdbc60c
AndrisBorbas <andris.borbas@gmail.com>
2026-01-20 17:37:57 +0100

    fix error

--- src/components/admin/login.tsx
+++ src/components/admin/login.tsx
@@ -64,7 +64,7 @@ export function AdminLogin({ onLogin, error }: AdminLoginProps) {
 
 					{error && (
 						<Field data-invalid>
-							<FieldError errors={[error]} />
+							<FieldError errors={[{ message: error }]} />
 						</Field>
 					)}
 
